daad98139185b2ec02d212a4efa3fcdc602b32ff
Ernesto <neto_neto@hotmail.com>
2016-07-25 15:56:22 -0500

    Add album seeder script with individual statements for each record

---
+++ albums_seeder.sql
@@ -0,0 +1,17 @@
+USE codeup_test_db
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Michael Jackson',	'Thriller',	'1982',	'Pop, rock, R&B', '45.4');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('AC/DC',	'Back in Black',	'1980',	'Hard rock', '25.9');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES ('Pink Floyd',	'The Dark Side of the Moon',	'1973',	'Progressive rock', '22.7');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES('Whitney Houston / Various artists',	'The Bodyguard',	'1992',	'Soundtrack/R&B, soul, pop', '27.4');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES('Meat Loaf',	'Bat Out of Hell',	'1977',	'Hard rock, progressive rock', '20.6');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES('Eagles',	'Their Greatest Hits (1971–1975)',	'1976',	'Rock, soft rock, folk rock', '32.2');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES('Bee Gees / Various artists',	'Saturday Night Fever',	'1977',	'Disco', '19');
+INSERT INTO albums (artist, name, release_date, genre, sales)
+VALUES('Fleetwood Mac',	'Rumours',	'1977',	'Soft rock', '27.9');
