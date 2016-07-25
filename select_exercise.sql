72a5558f36edbc2857e368fa88d953055ff021db
Ernesto <neto_neto@hotmail.com>
2016-07-25 16:41:22 -0500

    Moved explanations before output

--- select_exercise.sql
+++ select_exercise.sql
@@ -1,13 +1,13 @@
 USE codeup_test_db
-SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT 'These are all albums by Pink Floyd.' AS 'Result Explanation';
-SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT name FROM albums WHERE artist = 'Pink Floyd';
 SELECT 'This is Sgt. Pepper''s Lonely Hearts Club Band release date.' AS 'Result Explanation';
-SELECT genre FROM albums WHERE name = 'Nevermind';
+SELECT release_date FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
 SELECT 'This is the genre for the album Nevermind.' AS 'Result Explanation';
-SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT genre FROM albums WHERE name = 'Nevermind';
 SELECT 'These are all albums release in the 90''s.' AS 'Result Explanation';
-SELECT name FROM albums WHERE sales < 20;
+SELECT name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
 SELECT 'These are all albums with less than 20 million certified sales.' AS 'Result Explanation';
-SELECT name FROM albums WHERE genre = 'rock';
+SELECT name FROM albums WHERE sales < 20;
 SELECT 'These are all rock genre albums.' AS 'Result Explanation';
+SELECT name FROM albums WHERE genre = 'rock';
