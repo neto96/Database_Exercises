4ca83aff4341677b7fd6df0a1abada31c9fbed75
Ernesto <neto_neto@hotmail.com>
2016-07-25 14:57:52 -0500

    Albums table creation script

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS albums (
+id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+artist VARCHAR(50),
+name VARCHAR(50),
+release_date YEAR,
+sales FLOAT,
+genre VARCHAR(50),
+PRIMARY KEY (id)
+);
