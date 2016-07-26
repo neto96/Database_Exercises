db87343cdd020df9d7009d5b3898eea466ae0aab
Ernesto <neto_neto@hotmail.com>
2016-07-26 14:30:32 -0500

    Add delete exercise with SELECT statements

---
+++ delete_exercises.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db
+SELECT 'These are all the albums released after 1991' AS 'Result Explanation';
+SELECT '' AS '--------------------------------------';
+SELECT * FROM albums WHERE release_date > 1991;
+SELECT '' AS '--------------------------------------';
+SELECT 'These are all the albums with the genre disco' AS 'Result Explanation';
+SELECT '' AS '--------------------------------------';
+SELECT * FROM albums WHERE genre = 'disco';
+SELECT '' AS '--------------------------------------';
+SELECT 'These are all the albums by Whitney Houston' AS 'Result Explanation';
+SELECT '' AS '--------------------------------------';
+SELECT * FROM albums WHERE artist = 'Whitney Houston';
