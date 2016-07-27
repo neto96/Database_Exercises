8bf930cb9433141e305d019b94d9abeee6269a50
Ernesto <neto_neto@hotmail.com>
2016-07-27 12:26:05 -0500

    Update first query to order by first then last name

--- where_exercise.sql
+++ where_exercise.sql
@@ -1,4 +1,4 @@
-SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY first_name, last_name;
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31' AND birth_date LIKE '%-12-25';
