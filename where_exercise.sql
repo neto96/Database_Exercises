a8e7d4f145b6efa942c2f8f802ac2dc383217fab
Ernesto <neto_neto@hotmail.com>
2016-07-27 11:54:22 -0500

    Change first query from IN to OR

--- where_exercise.sql
+++ where_exercise.sql
@@ -1,4 +1,4 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
 SELECT * FROM employees WHERE last_name LIKE 'e%';
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31';
 SELECT * FROM employees WHERE birth_date LIKE '%-12-25';
