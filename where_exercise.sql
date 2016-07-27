90a43bbd28b0f6fb532c17e907971eb3ba22f023
Ernesto <neto_neto@hotmail.com>
2016-07-27 12:29:55 -0500

    Update queries searching for employees with 'E' to sort by employee number

--- where_exercise.sql
+++ where_exercise.sql
@@ -1,5 +1,5 @@
 SELECT * FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') ORDER BY last_name, first_name;
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
-SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e'ORDER BY emp_no;
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-1-1' AND '1999-12-31' AND birth_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
