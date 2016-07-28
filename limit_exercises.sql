05bce281eb9bc9962cccf42e16659d3ee9eb8946
Ernesto <neto_neto@hotmail.com>
2016-07-28 09:56:27 -0500

    Add limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT last_name FROM employees GROUP BY last_name DESC LIMIT 10;
+SELECT first_name, last_name FROM employees WHERE birth_date LIKE '%-12-25' AND hire_date BETWEEN '1990-1-1' AND '1999-12-31' ORDER BY birth_date ASC, hire_date DESC LIMIT 5 OFFSET 45;
